5d1e6c0db4f8d3ea6be6498777c0eb62000cc3fc
Amit <amitwaghmare1211@gmail.com>
2026-02-08 17:50:47 +0530

    Add MERN auth backend with JWT

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const authRoutes = require("./routes/authRoutes");
+
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("MERN Auth API running...");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB Connected");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
+  .catch((err) => console.log(err));
